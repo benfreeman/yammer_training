e6e3d7e0cf446e213fb8ffbc20ba2d5c64ee0556
Ben Freeman <bfreeman@yammer-inc.com>
2016-01-21 00:11:42 -0800

    initial commit

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,19 @@
+module UsersHelper
+  def gravatar_for(user, options = { :size => 50 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.full_name, class: "gravatar", :height => options[:size], :width => options[:size])
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+end
