cf735058104a1b47a6ff8bcb0505efb92aa525f3
Ben Freeman <bfreeman@yammer-inc.com>
2016-02-09 09:00:40 -0800

    static pages content and help link in footer

--- ben_notes.md
+++ ben_notes.md
@@ -10,3 +10,16 @@ Heroku
   http://stackoverflow.com/questions/21544626/how-do-you-manage-secret-keys-and-heroku-with-ruby-on-rails-4-1-0beta1/22458102#22458102
   https://github.com/laserlemon/figaro
 pg vs sqlite
+routes
+rest / crud
+
+Chapter 2
+validations
+associations
+migrations
+
+Chapter 3
+static pages
+try to get /help routing to your static pages help page
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
   get 'static_pages/home'
+  get 'help' => 'static_pages#help', as: :help
+  get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  get 'static_pages/help'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  root 'welcome#index'
   devise_for :users
   resources :articles
   resources :users
