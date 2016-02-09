d428f91c7ab4e030fd55e2f86f7ef82b887d0fd2
Ben Freeman <bfreeman@yammer-inc.com>
2016-02-08 21:26:50 -0800

    associate user/articles and add some validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+  validates :body, length: { minimum: 100 }, presence: true
+  validates :name, length: { minimum: 1 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :articles
+  validates :full_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
